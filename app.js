d0bccf8837e7c8f068cb1641559c10e22277d2bc
erickslk <erick.osorto@unitec.edu>
2023-08-16 19:12:31 -0600

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.use('/api/pais', pais);
 const direccion = require('./routes/direccion.js');
 app.use('/api/direccion', direccion);
 
+const rol = require('./routes/rol.js');
+app.use('/api/rol', rol);
+
+const usuario = require('./routes/usuario.js');
+app.use('/api/usuario', usuario);
 
 // Inicio del servidor
 const puerto = 3000;
