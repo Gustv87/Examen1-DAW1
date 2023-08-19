d4570016b894c6e4dd01f74921ed7aab255ee859
gustavomartinez <gustavomartinezh87@gmail.com>
2023-08-18 19:38:43 -0600

    Se actualzo direccion que solo reciba id_ciudad

--- routes/ciudad.js
+++ routes/ciudad.js
@@ -7,13 +7,17 @@ ciudad.post('/', (req, res) => {
         return;
     }
     const nombreciudad = req.body.nombre;
-    let datos = [nombreciudad];
-    let sql = `INSERT INTO tbl_ciudad (nombre) VALUES ($1) RETURNING id_ciudad`;
+    const idpais = req.body.id_pais;
+    let datos = [nombreciudad, idpais];
+    let sql = `INSERT INTO tbl_ciudad (nombre, id_pais) VALUES ($1, $2) RETURNING id_ciudad, id_pais`;
     db.one(sql, datos)
         .then(data => {
             const objetoCreado = {
                 id_ciudad: data.id_ciudad,
+                id_pais: data.id_pais,
                 nombre: nombreciudad,
+                id_pais: idpais
+
             };
             res.json(objetoCreado);
         })
@@ -22,6 +26,9 @@ ciudad.post('/', (req, res) => {
             res.status(500).json({ error: 'Error en la consulta a la base de datos' });
         });
 });
+// SELECT * FROM tbl_ciudad as c
+//     Inner join tbl pais as p on c.id_ciudad = p.id_pais
+//     where c.activo = true`;
 ciudad.get('/', (req, res) => {
     let sql = "SELECT * FROM tbl_ciudad  where activo = true";
     db.any(sql, e => e.id)
@@ -33,20 +40,25 @@ ciudad.get('/', (req, res) => {
             res.status(500).json({ error: 'Error en la consulta a la base de datos' });
         });
 });
+//
 ciudad.put('/:id', (req, res) => {
     const id_ciudad = req.params.id;
-    const { nombre } = req.body;
-    const parametros = [nombre, id_ciudad];
+    
+    const { nombre, idpais} = req.body;
+    const parametros = [nombre, idpais];
     const sql = `
       UPDATE tbl_ciudad 
-      SET nombre = $1
-      WHERE id_ciudad = $2
+      SET nombre = $1,
+      WHERE id_pais = $2
+
+      WHERE id_ciudad = $3
     `;
     db.query(sql, parametros)
         .then(data => {
             const objetoModificado = {
                 id_ciudad: id_ciudad,
-                nombre: nombre
+                nombre: nombre,
+                id_pais:idpais
             };
             res.json(objetoModificado);
         });
