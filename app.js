beaea6f98d66f68b757e660d2bbc3bc967069ef4
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2023-09-01 19:34:37 -0600

    Update app.js
    Permisos de Consumo Externo

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
+});
+
+
 //Rutas
 const ciudad = require('./routes/ciudad.js');
 app.use('/api/ciudad', ciudad);
