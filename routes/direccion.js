78af802d6b03bd3cf464e24c7d09e5508fd1f09a
gustavomartinez <gustavomartinezh87@gmail.com>
2023-09-05 23:09:31 -0600

    se actualizo la tabla de direcciones

--- routes/direccion.js
+++ routes/direccion.js
@@ -2,58 +2,52 @@ const express = require('express');
 const direccion = express.Router();
 const db = require('../db/conn');
 const pgp = require('pg-promise')();
-direccion.post('/', (req, res) => {
-    if (!req.body.direccion || !req.body.descripcion || !req.body.correo || !req.body.id_ciudad || !req.body.id_pais) {
+
+direccion.post('/', async (req, res) => {
+    if (!req.body.direccion || !req.body.descripcion || !req.body.correo || !req.body.id_ciudad) {
         res.status(400).json({ error: 'Faltan campos obligatorios' });
         return;
     }
     const { direccion, descripcion, correo, id_ciudad } = req.body;
     let datos = [direccion, descripcion, correo, id_ciudad];
     let sql = `INSERT INTO tbl_direccion (direccion, descripcion, correo, id_ciudad) VALUES ($1, $2, $3, $4) RETURNING id_direccion`;
-    db.one(sql, datos)
-        .then(data => {
-            const objetoCreado = {
-                id_direccion: data.id_direccion,
-                direccion: direccion,
-                descripcion: descripcion,
-                correo: correo,
-                id_ciudad: id_ciudad,
 
-            };
-            res.json(objetoCreado);
-        })
-        .catch(error => {
-            console.error(error);
-            res.status(500).json({ error: 'Error en la consulta a la base de datos' });
-        });
-});
-direccion.get('/', async (req, res) => {
     try {
-        // Consulta para obtener las direcciones activas
-        const direccionSql = "SELECT * FROM tbl_direccion WHERE activo = true";
-        const direcciones = await db.any(direccionSql);
+        const data = await db.one(sql, datos);
 
-        // Consulta para obtener los nombres de las ciudades según sus id_ciudad
-        const idCiudades = direcciones.map((direccion) => direccion.id_ciudad);
-        const ciudadSql = pgp.as.format("SELECT id_ciudad, nombre FROM tbl_ciudad WHERE id_ciudad IN ($1:csv)", [idCiudades]);
-        const ciudades = await db.any(ciudadSql);
+        // Ahora, obtén el nombre de la ciudad correspondiente
+        const ciudadSql = 'SELECT nombre FROM tbl_ciudad WHERE id_ciudad = $1';
+        const ciudadData = await db.one(ciudadSql, id_ciudad);
 
-        // Mapear los resultados para agregar el nombre de la ciudad a cada dirección
-        const direccionesConCiudad = direcciones.map((direccion) => {
-            const ciudad = ciudades.find((ciudad) => ciudad.id_ciudad === direccion.id_ciudad);
-            return {
-                ...direccion,
-                nombre_ciudad: ciudad ? ciudad.nombre : null,
-            };
-        });
-
-        res.setHeader('Content-Type', 'application/json');
-        res.json(direccionesConCiudad);
+        const objetoCreado = {
+            id_direccion: data.id_direccion,
+            direccion: direccion,
+            descripcion: descripcion,
+            correo: correo,
+            id_ciudad: ciudadData.nombre, // Mantenemos el nombre de campo 'id_ciudad'
+        };
+        
+        res.json(objetoCreado);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Error en la consulta a la base de datos' });
     }
 });
+
+direccion.get('/', async (req, res) => {
+    try {
+        // Consulta para obtener las direcciones activas
+        const direccionSql = "SELECT d.*, c.nombre AS nombre_ciudad FROM tbl_direccion d INNER JOIN tbl_ciudad c ON d.id_ciudad = c.id_ciudad WHERE d.activo = true";
+        const direcciones = await db.any(direccionSql);
+
+        res.setHeader('Content-Type', 'application/json');
+        res.json(direcciones);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error en la consulta a la base de datos' });
+    }
+});
+
 direccion.put('/:id', (req, res) => {
     const idDireccion = req.params.id;
     const { direccion, descripcion, id_ciudad } = req.body;
@@ -61,7 +55,7 @@ direccion.put('/:id', (req, res) => {
         res.status(400).json({ error: 'Faltan campos obligatorios' });
         return;
     }
-    const parametros = [direccion, descripcion, id_ciudad, id_pais, idDireccion];
+    const parametros = [direccion, descripcion, id_ciudad, idDireccion];
     const sql = `
       UPDATE tbl_direccion 
       SET direccion = $1, descripcion = $2, id_ciudad = $3
