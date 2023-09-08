119dc1783a43b154d310e10c2992689a6be3af01
gustavomartinez <gustavomartinezh87@gmail.com>
2023-09-08 17:58:00 -0600

    se agrego login y arreglo usuarios

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,38 +3,42 @@ const usuario = express.Router();
 const db = require('../db/conn');
 
 usuario.post('', (req, res) => {
-    let params = [
-        req.body.correo,
-        req.body.nombre,
-        req.body.id_rol
-    ];
-    let sql = `INSERT INTO tbl_usuario (correo, nombre, id_rol) values ($1, $2, $3) 
-                                                            RETURNING correo`;
-    console.log(params)
-    db.one(sql, params, event => event.id_rol)
-        .then(data => {
+    let correo = req.body.correo;
+    let nombre_usuario = req.body.nombre_usuario;
+    let password = req.body.password;
+    let id_rol = 2;
+  
+    // Verificar si el correo electrónico ya existe en la base de datos
+    let sqlVerificarCorreo = `SELECT correo FROM tbl_usuario WHERE correo = $1`;
+    db.one(sqlVerificarCorreo, [correo])
+      .then(() => {
+        // El correo electrónico ya existe, devolver un error
+        res.status(400).json({ error: 'El correo electrónico ya está registrado' });
+      })
+      .catch(() => {
+        // El correo electrónico no existe, realizar la inserción
+        let sqlInsertarUsuario = `INSERT INTO tbl_usuario (correo, nombre, password, id_rol) VALUES ($1, $2, $3, $4) RETURNING correo, id_rol`;
+        let params = [correo, nombre_usuario, password, id_rol];
+  
+        db.one(sqlInsertarUsuario, params)
+          .then(data => {
             const objetoCreado = {
-                correo: params[0],
-                nombre: params[1],
-                id_rol: params[2]
-            }
+              correo: data.correo,
+              nombre_usuario: nombre_usuario,
+              password: password,
+              id_rol: data.id_rol
+            };
             res.json(objetoCreado);
-        })
-        .catch((error) => {
+          })
+          .catch((error) => {
             res.status(500).json({ error: 'Error en la consulta a la base de datos' });
-        });
-});
+          });
+      });
+  });
 usuario.get('', (req, res) => {
-    let sql = `SELECT
-    a.correo,
-    a.nombre,
-    b.id_rol,
-    b.nombre as rol
-FROM
-    tbl_usuario a
-    inner join tbl_rol b on b.id_rol = b.id_rol
-where
-    a.activo = true
+    let sql = `SELECT tbl_usuario.correo, tbl_usuario.nombre AS nombre_usuario, tbl_rol.nombre AS nombre_rol
+    FROM tbl_usuario
+    INNER JOIN tbl_rol ON tbl_usuario.id_rol = tbl_rol.id_rol;
     `;
     db.any(sql, e => e.correo)
         .then(rows => {
