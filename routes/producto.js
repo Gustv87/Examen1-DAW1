c3227abf631c0258f3160b0817a6287803bb1f4e
mariel2494 <dinorammontoya@hotmail.com>
2023-09-08 19:29:37 -0600

    prodcutos

--- routes/producto.js
+++ routes/producto.js
@@ -1,38 +1,65 @@
 const express = require('express');//const que es nuestra libreria de express
 const producto = express.Router();
 const db = require('../db/conn');//conexion a la base de datos
-producto.post('', (req, res) => {
-    let datos = [
-        req.body.nombre,
-        req.body.precio
-    ];
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+const upload = multer({storage});
+
+producto.post('',  upload.single('foto')  , (req, res)=>{
+
+
+    if ( !req.file ){
+        return res.status(500).json( {error:'Debes enviar un archivo'} );
+    }
+
+    const { nombre, precio } = req.body; 
+
+    const valores = [nombre, precio, req.file.buffer];
+
+
     let sql = ` insert into tbl_producto
-                (nombre, precio)
-                values
-                ($1, $2) returning id_producto`;
-    db.one(sql, datos, event => event.id_producto)
-        .then(data => {
-            const objetoCreado = {
-                id_producto: data,
-                nombre: datos[0],
-                precio: datos[1]
-            }
+                ( nombre, precio,foto)
+                values 
+                ( $1, $2 ,$3) returning id_producto , nombre `;
+
+    db.one(sql , valores)
+        .then( data =>{
+
+            let objetoCreado = { 
+                                id : data.id_producto, 
+                                nombre : data.nombre, 
+                                precio: precio,
+                                
+                            };
             res.json(objetoCreado);
+
         })
-        .catch((error) => {
-            res.json(error);
-        });
+        .catch( error=>{
+            res.status(500).json(error);
+        })
+
 });
-producto.get('', (req, res) => {
-    let sql = "select * from tbl_producto where activo = true";
-    db.any(sql, e => e.id_producto)
-        .then(rows => {
-            res.json(rows);
-        })
-        .catch((error) => {
-            res.json(error);
-        })
-})
+
+producto.get('', (req, res)=>{
+
+    let sql = ` select id_producto , nombre,precio,  encode(foto, 'base64') foto from tbl_producto where activo = true `;
+
+    db.any(sql)
+    .then( rows=>{
+
+        res.json(rows);
+
+    })
+    .catch( error=>{
+
+        res.status(500).json(error);
+
+    } )
+
+});
+
+
 producto.put('/:id_producto', (req, res) => {
     const parametros = [
         req.body.nombre,
